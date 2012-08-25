04e46b557aa90adbbfc590e22749a314f99d1d5e
Bart Veldstra <bart.veldstra@gmail.com>
2012-08-25 14:52:02 +0200

    Changed fire to 'Z' to prevent jamming

--- keyboard.py
+++ keyboard.py
@@ -3,7 +3,7 @@ from pyglet.window import key
 keystate = None
 
 UP, DOWN, LEFT, RIGHT = key.UP, key.DOWN, key.LEFT, key.RIGHT
-FIRE = key.SPACE
+FIRE = key.Z
 
 def key_down(key_id):
     return keystate[key_id]
