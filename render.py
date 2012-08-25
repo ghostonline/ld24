7183c6a4df0a8192aaeb573a0e3bb7e67a3bff89
Bart Veldstra <bart.veldstra@gmail.com>
2012-08-25 12:00:21 +0200

    Use an offset when placing a sprite

--- render.py
+++ render.py
@@ -6,6 +6,11 @@ renderables = {}
 images = {}
 sprite_batch = pyglet.graphics.Batch()
 
+class SpriteState:
+    def __init__(self, sprite, offset):
+        self.sprite = sprite
+        self.offset = offset
+
 def add_component(entity_id, image_name):
     assert entity_id not in renderables
 
@@ -16,15 +21,22 @@ def add_component(entity_id, image_name):
     images[image_name] = image_res
 
     sprite = pyglet.sprite.Sprite(image_res, batch=sprite_batch)
-    renderables[entity_id] = sprite
+    offset = image_res.width * 0.5, image_res.height * 0.5
+    renderables[entity_id] = SpriteState(sprite, offset)
 
 def set_rotation(entity_id, angle):
     sprite = renderables[entity_id]
     sprite.rotation = angle
 
 def update():
-    for entity_id, sprite in renderables.iteritems():
-        sprite.x, sprite.y = spatial.get_position(entity_id)
+    for entity_id, state in renderables.iteritems():
+        x, y = spatial.get_position(entity_id)
+        offset = state.offset
+        x -= offset[0]
+        y -= offset[0]
+        sprite = state.sprite
+        sprite.x = x
+        sprite.y = y
 
 def draw():
     window.clear()
