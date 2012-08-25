ca1c5aba8ea9beb19ea5abc6d5ee5e390feb8636
Bart Veldstra <bart.veldstra@gmail.com>
2012-08-25 19:50:00 +0200

    Allow enemy ships to be collected when low on health

--- evoseed.py
+++ evoseed.py
@@ -1,4 +1,4 @@
-import health, spatial
+import health, spatial, collider, manager
 
 DEFAULT_COLLECTOR_THRESHOLD = 100
 DRIFT_SPEED = 50
@@ -6,6 +6,7 @@ DRIFT_SPEED = 50
 evoseeds = {}
 
 collector_id = None
+collect_events = []
 
 def add_component(entity_id, max_health):
     evoseeds[entity_id] = max_health
@@ -26,3 +27,21 @@ def update(dt):
             drift_direction = (collector_position - position).normalized()
             drift = drift_direction * pull_strength * DRIFT_SPEED * dt
             spatial.move_vec(entity_id, drift)
+
+    for entity_id in collect_events:
+        print "Collected %d" % entity_id
+        manager.destroy_entity(entity_id)
+
+def process_events():
+    global collect_events
+    collect_events = []
+
+    seed_ids = collider.collide_events.intersection(evoseeds)
+    for seed_id in seed_ids:
+        collide_ids = collider.collide_events_data[seed_id]
+        if collector_id in collide_ids:
+            current_health = health.get_health(seed_id)
+            max_health = evoseeds[seed_id]
+            if current_health > max_health:
+                continue
+            collect_events.append(seed_id)
